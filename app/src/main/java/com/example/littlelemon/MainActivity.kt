3a7cadcce3aa489598fd14075918d14873d1f8fc
unknown <garimella.balu@gmail.com>
2024-05-03 11:35:19 +0530

    image added

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.littlelemon.ui.theme.LittlelemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +37,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+    Column {
+        Image(modifier = Modifier.size(300.dp),
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Cartoon of dog")
+    }
 }
 
 @Preview(showBackground = true)
