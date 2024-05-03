327833e5152566f127caaf721439dc1dd73820e7
unknown <garimella.balu@gmail.com>
2024-05-03 15:08:28 +0530

    adding text feilds

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -6,15 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.littlelemon.ui.theme.LittlelemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +38,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var firstNameText by remember { mutableStateOf("first name") }
+    var lastNameText by remember { mutableStateOf("last name") }
+    var emailAddressText by remember { mutableStateOf("email address") }
     Column {
         Image(modifier = Modifier.size(300.dp),
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "Cartoon of dog")
+        Text(
+            style = TextStyle(background = Color.Yellow),
+            modifier = Modifier.padding(20.dp),
+            text = "Let's get to know you",
+            fontSize = 24.sp)
+        TextField(
+            value = firstNameText,
+            onValueChange = { firstNameText = it },
+            label = { Text("First Name") }
+        )
+        TextField(
+            value = lastNameText,
+            onValueChange = { lastNameText = it },
+            label = { Text("Last Name") }
+        )
+        TextField(
+            value = emailAddressText,
+            onValueChange = { emailAddressText = it },
+            label = { Text("Email") }
+        )
+        Button(onClick = {  }) {
+            Text("Register")
+        }
     }
 }
 
