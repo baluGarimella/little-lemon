7c51503dbad568b0fb67f47bb643adbb77011e4a
unknown <garimella.balu@gmail.com>
2024-05-15 06:55:25 +0530

    network

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -17,10 +18,20 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.littlelemon.ui.theme.LittlelemonTheme
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val responseLiveData = MutableLiveData<String>()
+    private val httpClient = HttpClient(Android)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,11 +41,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val navController = rememberNavController()
-                   Navigation(navController = navController)
+                    val responseState = responseLiveData.observeAsState("").value
+
+                   // val navController = rememberNavController()
+                  // Navigation(navController = navController)
+                    Column {
+                        Button(
+                            onClick = {
+                                lifecycleScope.launch {
+                                    val response = fetchContent()
+                                    runOnUiThread {
+                                        responseLiveData.value = response
+                                    }
+                                }
+                            }
+                        ) {
+                            Text(text = "Download")
+                        }
+
+                        Text(text = responseState.toString())
+                    }
                 }
             }
         }
+    }
+    private suspend fun fetchContent(): String {
+        return httpClient
+            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
+            .bodyAsText()
     }
 }
 
