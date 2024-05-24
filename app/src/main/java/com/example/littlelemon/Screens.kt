7cc121dd0c0f88469433313bff4256a04397786f
baluGarimella <78028186+baluGarimella@users.noreply.github.com>
2024-05-24 10:30:12 +0530

    Update Screens.kt

--- app/src/main/java/com/example/littlelemon/Screens.kt
+++ app/src/main/java/com/example/littlelemon/Screens.kt
@@ -1,2 +1,7 @@
 package com.example.littlelemon
 
+sealed class Screens (val rout : String) {
+    data object Onboarding : Screens("Onboarding")
+    data object Home : Screens("Home")
+    data object Profile : Screens("Profile")
+}
