c84646198f7a5a4a9e2749f10c5521e7ad57a47d
baluGarimella <78028186+baluGarimella@users.noreply.github.com>
2024-05-24 10:33:16 +0530

    Update Onboarding.kt

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -18,55 +18,128 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
+import android.content.SharedPreferences
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Onboarding(navController: NavHostController) {
-    val context: Context = LocalContext.current
-    val preferencesManager = remember { PreferencesManager(context) }
-
-    var firstNameText by remember { mutableStateOf("") }
-    var lastNameText by remember { mutableStateOf("") }
-    var emailAddressText by remember { mutableStateOf("") }
-    var errorText by remember { mutableStateOf("") }
-    Column {
-        Image(modifier = Modifier.size(300.dp),
+fun Onboarding(navController: NavHostController, prefs: SharedPreferences) {
+    val firstName = remember {
+        mutableStateOf("")
+    }
+    val lastName = remember {
+        mutableStateOf("")
+    }
+    val emailAddress = remember {
+        mutableStateOf("")
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
             painter = painterResource(id = R.drawable.logo),
-            contentDescription = "Cartoon of dog")
+            contentDescription = "Little Lemon logo",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp),
+            alignment = Alignment.TopCenter
+        )
         Text(
-            style = TextStyle(background = Color.Yellow),
-            modifier = Modifier.padding(20.dp),
-            text = "Let's get to know you",
-            fontSize = 24.sp)
-        TextField(
-            value = firstNameText,
-            onValueChange = { firstNameText = it },
-            label = { Text("First Name") },
+            text = "Let's get to know you", modifier = Modifier
+                .fillMaxWidth()
+                .background(color = colorResource(id = R.color.primary_green))
+                .padding(20.dp), textAlign = TextAlign.Center,
+            color = colorResource(id = R.color.highlight_light),
+            fontSize = 48.sp,
+            lineHeight = 58.sp
         )
-        TextField(
-            value = lastNameText,
-            onValueChange = { lastNameText = it },
-            label = { Text("Last Name") }
-        )
-        TextField(
-            value = emailAddressText,
-            onValueChange = { emailAddressText = it },
-            label = { Text("Email") }
-        )
-        Button(onClick = {
-            if (firstNameText.isNotBlank() || lastNameText.isNotBlank() || emailAddressText.isNotBlank()) {
-                preferencesManager.firstName = firstNameText
-                preferencesManager.lastName = lastNameText
-                preferencesManager.email = emailAddressText
-                navController.navigate(Home.route)
-            } else {
-                errorText = "Registration unsuccessful. Please enter all data."
-            }
-            }) {
-            Text("Register")
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1.0f)
+                .padding(20.dp), verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Personal information".uppercase(),
+                modifier = Modifier.padding(vertical = 20.dp),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+            TextField(
+                value = firstName.value,
+                onValueChange = { firstName.value = it },
+                label = { Text(text = "First name*") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp)
+            )
+            TextField(
+                value = lastName.value,
+                onValueChange = { lastName.value = it },
+                label = { Text(text = "Last name*") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp)
+            )
+            TextField(
+                value = emailAddress.value,
+                onValueChange = { emailAddress.value = it },
+                label = { Text(text = "Email address*") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
         }
-        Text(
-            text = errorText,
-            color = Color.Red,
-        )
-
+        Button(
+            onClick = {
+                if (firstName.value.isNotBlank() && lastName.value.isNotBlank() && emailAddress.value.isNotBlank()) {
+                    if (prefs.edit().putString("firstName", firstName.value)
+                            .putString("lastName", lastName.value).putString("emailAddress", emailAddress.value)
+                            .putBoolean("logged", true).commit()
+                    ) {
+                        navController.popBackStack(Screens.Onboarding.rout, true)
+                        navController.navigate(Screens.Home.rout)
+                    }
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.primary_yellow),
+                contentColor = colorResource(id = R.color.highlight_dark)
+            ), shape = RoundedCornerShape(16.dp)
+        ) { Text(text = "Register") }
     }
 }
