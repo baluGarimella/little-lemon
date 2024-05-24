6430fd5b992304aef14c1b53b53bbfe6a1343a8b
baluGarimella <78028186+baluGarimella@users.noreply.github.com>
2024-05-24 10:35:14 +0530

    Update LittleLemonNavigation.kt

--- app/src/main/java/com/example/littlelemon/LittleLemonNavigation.kt
+++ app/src/main/java/com/example/littlelemon/LittleLemonNavigation.kt
@@ -1,2 +1,25 @@
 package com.example.littlelemon
 
+import android.content.SharedPreferences
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun Navigation(navController: NavHostController, prefs: SharedPreferences, database: LittleLemonDatabase) {
+    NavHost(
+        navController = navController,
+        startDestination = (if (prefs.getBoolean("logged", false)) Screens.Home.rout else Screens.Onboarding.rout)
+    ) {
+        composable(Screens.Onboarding.rout) {
+            Onboarding(navController, prefs)
+        }
+        composable(Screens.Home.rout) {
+            Home(navController, database)
+        }
+        composable(Screens.Profile.rout) {
+            Profile(navController, prefs)
+        }
+    }
+}
