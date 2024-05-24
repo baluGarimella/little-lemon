6926ce0854a6889b2019b77b655573c7b0d85f54
baluGarimella <78028186+baluGarimella@users.noreply.github.com>
2024-05-24 10:33:52 +0530

    Update Network.kt

--- app/src/main/java/com/example/littlelemon/Network.kt
+++ app/src/main/java/com/example/littlelemon/Network.kt
@@ -4,45 +4,28 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-
-data class MenuNetwork(
-
-    @SerialName("menu")
-    val menu: List<MenuItemNetwork>,
-
-    )
+class MenuNetworkData(@SerialName("menu") val menu: List<MenuItemNetwork>)
 
 @Serializable
-
 data class MenuItemNetwork(
-
     @SerialName("id")
     val id: Int,
-
     @SerialName("title")
     val title: String,
-
     @SerialName("description")
     val description: String,
-
-
     @SerialName("price")
-    val price: Double,
-
+    val price: Int,
     @SerialName("image")
     val image: String,
-
     @SerialName("category")
-    val category: String,
-
-    ) {
-    fun toMenuItemRoom() = MenuItemRoom(
-        id,
-        title,
-        description,
-        price,
-        image,
-        category
-    )
-
-}
+    val category: String
+){
+    fun toMenuItemRoom()  = MenuItemDatabase(
+        id = id,
+        title = title,
+        price = price,
+        description = description,
+        image = image,
+        category = category
+    )}
