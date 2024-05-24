b0ce45cca2462e5289bb9c851ae414173e565468
baluGarimella <78028186+baluGarimella@users.noreply.github.com>
2024-05-24 10:37:04 +0530

    Update Database.kt

--- app/src/main/java/com/example/littlelemon/Database.kt
+++ app/src/main/java/com/example/littlelemon/Database.kt
@@ -1,31 +1,77 @@
 package com.example.littlelemon
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-@Entity
-data class MenuItemRoom(
-    @PrimaryKey val id: Int,
-    val title: String,
-    val description: String,
-    val price: Double,
-    val image: String,
-    val category: String,
-)
+
+@Database(entities = [MenuItemDatabase::class], version = 1, exportSchema = false)
+abstract class LittleLemonDatabase : RoomDatabase() {
+    abstract fun menuDao(): MenuDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LittleLemonDatabase? = null
+
+        fun getDatabase(context: Context): LittleLemonDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    LittleLemonDatabase::class.java,
+                    "little_lemon_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
 
 @Dao
-interface MenuItemDao {
-    @Query("SELECT * FROM MenuItemRoom")
-    fun getAll(): LiveData<List<MenuItemRoom>>
+interface MenuDao {
+    suspend fun insertItems(list: kotlin.collections.List<MenuItemDatabase>) {
+        for (item in list) {
+            insertItem(
+                MenuItemDatabase(
+                    item.id,
+                    item.title,
+                    item.description,
+                    item.price,
+                    item.image,
+                    item.category
+                )
+            )
+        }
+    }
 
-    @Insert
-    fun insertAll(vararg menuItems: MenuItemRoom)
+    @Insert(entity = MenuItemDatabase::class )
+    suspend fun insertItem(item: MenuItemDatabase)
 
-    @Query("SELECT (SELECT COUNT(*) FROM MenuItemRoom) == 0")
+    @Query("SELECT * FROM menu_item_table")
+    fun getItems(): LiveData<List<MenuItemDatabase>>
+    @Query("SELECT (SELECT COUNT(*) FROM  menu_item_table) == 0")
     fun isEmpty(): Boolean
 }
 
-@Database(entities = [MenuItemRoom::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun menuItemDao(): MenuItemDao
-}
+@Entity(tableName = "menu_item_table")
+data class MenuItemDatabase(
+    @PrimaryKey
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Int,
+    val image: String,
+    val category: String
+)
