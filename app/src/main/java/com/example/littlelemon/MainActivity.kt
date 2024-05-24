eff3a526e5542fd0b3edbbbf63d0e55830229f28
baluGarimella <78028186+baluGarimella@users.noreply.github.com>
2024-05-24 10:34:43 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,156 +1,68 @@
 package com.example.littlelemon
 
 import android.os.Bundle
-import androidx.compose.ui.Alignment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import io.ktor.http.ContentType
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
+import com.example.littlelemon.LittleLemonDatabase.Companion.getDatabase
 import com.example.littlelemon.ui.theme.LittlelemonTheme
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.android.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.serialization.kotlinx.json.*
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+//Little_LemonTheme
 class MainActivity : ComponentActivity() {
-    private val menuItemsLiveData = MutableLiveData<MenuNetwork>()
-    private val httpClient = HttpClient(Android) {
-        install(ContentNegotiation) {
+    private val httpClient = HttpClient(Android){
+        install(ContentNegotiation){
             json(contentType = ContentType("text", "plain"))
         }
     }
-
-    private val database by lazy {
-        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
-    }
-
+    private val database by lazy { getDatabase(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             LittlelemonTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    val navHostController = rememberNavController()
+                    val prefs = getSharedPreferences("LittleLemon", MODE_PRIVATE)
 
-                val databaseMenuItems by database.menuItemDao().getAll().observeAsState(emptyList())
-
-                var orderMenuItems by remember { mutableStateOf(false) }
-
-                var menuItems = if (orderMenuItems) {
-                    databaseMenuItems.sortedBy { it.title }
-                } else {
-                    databaseMenuItems
-                }
-
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.logo),
-                        contentDescription = "logo",
-                        modifier = Modifier.padding(50.dp)
-                    )
-
-                    Button(
-                        onClick = { orderMenuItems = true },
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "Tap to Order By Name")
-                    }
-
-                    var searchPhrase by remember { mutableStateOf("") }
-
-                    OutlinedTextField(
-                        value = searchPhrase,
-                        onValueChange = { searchPhrase = it },
-                        label = {
-                            Text(text = "Search")
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 50.dp, end = 50.dp)
-                    )
-
-                    if (searchPhrase.isNotEmpty()) {
-                        val filteredItems = menuItems.filter {
-                            it.title.contains(searchPhrase, true)
-                        }
-                        menuItems = filteredItems
-                    }
-
-                    MenuItemsList(menuItems)
+                    Navigation(navController = navHostController,prefs, database = database)
                 }
             }
         }
-
         lifecycleScope.launch(Dispatchers.IO) {
-            if (database.menuItemDao().isEmpty()) {
-                val menu = getMenu()
-                saveMenuToDatabase(menu)
+            if(database.menuDao().isEmpty()) {
+                saveMenuToDatabase(fetchMenu())
             }
         }
     }
-    private suspend fun getMenu(): List<MenuItemNetwork> {
-        val response: MenuNetwork = httpClient
-            .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
-            .body()
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val url = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        val response = httpClient.get(url).body<MenuNetworkData>()
         return response.menu
     }
 
-    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+    private suspend fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
         val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
-        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
+        database.menuDao().insertItems(menuItemsRoom)
     }
-}
 
-@Composable
-private fun MenuItemsList(items: List<MenuItemRoom>) {
-    /*LazyColumn(
-        modifier = Modifier
-            .fillMaxHeight()
-            .padding(top = 20.dp)
-    ) {
-        items(
-            items = items,
-            itemContent = { menuItem ->
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Text("")
-                    Text(
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(5.dp),
-                        textAlign = TextAlign.Right,
-                        text = "%.2f".format("")
-                    )
-                }
-            }
-        )
-    }*/
-}
 
+}
 
 
